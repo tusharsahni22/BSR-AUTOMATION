eb6aaa855cd6f05b4cb03c6e01e2379aac66d080
Tushar Sahni <tusharsahni22@gmail.com>
2022-07-07 00:49:30 +0530

    CompanyProfile done

---
+++ src/Module/CompanyProfile/SlideShow/slide.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./style.css";
+import { Slide } from 'react-slideshow-image';
+
+const slideImages = [
+    './dcdc.jpg',
+    './dcdc.jpg',
+    './dcdc.jpg'
+];
+
+const Slideshow = () => {
+    return (
+      <div>
+        <Slide easing="ease">
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    )
+};
+
+export default Slideshow;
